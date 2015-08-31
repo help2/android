389a1622fc2d309e923f0fd433345d3a7339f1f7
Lev Stipakov <lstipakov@gmail.com>
2015-08-31 21:07:39 +0300

    First version

---
+++ app/src/main/java/fi/stipakov/heartproject/Place.java
@@ -0,0 +1,23 @@
+package fi.stipakov.heartproject;
+
+import java.util.List;
+
+/**
+ * Created by stipa on 29.8.15.
+ */
+public class Place {
+    String name;
+    String id;
+    String phone;
+    String hours;
+    String website;
+    String person;
+    String street;
+    String city;
+    String zipcode;
+    double lat;
+    double lon;
+    double dist;
+
+    List<String> items;
+}
